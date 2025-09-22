e2b34cdab4cdb5db72932dff7e224e1ba4b2a325
Vivian t <viviantu789@gmail.com>
2025-09-21 23:28:22 -0400

    Activity 1.4 added email verification that its utoronto

--- hello4.py
+++ hello4.py
@@ -1,12 +1,13 @@
+import email
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email
 
-import os
+import os, re
 from os import environ
 
 app = Flask(__name__)
@@ -16,7 +17,11 @@ moment = Moment(app)
 app.config['SECRET_KEY'] = 'hoauefafhaeocahwoUHOUHAEFaefhoeufhFhfahoawfha'
 
 class NameForm(FlaskForm):
+    def validate_uoft_email(form, field):
+        if 'utoronto' not in field.data:
+            raise ValidationError('Please use your UofT email address')
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[DataRequired(), Email(), validate_uoft_email])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,9 +32,10 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index2.html',
-        form = form, name = session.get('name'))
+        form = form, email = session.get('email'), name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
